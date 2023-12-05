7f73eff29755ad97d7ffad8289a323ac71ec9bec
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-05 09:09:25 +0900

    lossの箇所print

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -381,7 +381,7 @@ class DDPM(pl.LightningModule):
         return loss
 
     def p_losses(self, x_start, t, noise=None):
-        "===================loss===================="
+        print("===================loss-pre====================")
         noise = default(noise, lambda: torch.randn_like(x_start))
         x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
         model_out = self.model(x_noisy, t)
@@ -887,6 +887,9 @@ class LatentDiffusion(DDPM):
         noise = default(noise, lambda: torch.randn_like(x_start))
         x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
         model_output = self.apply_model(x_noisy, t, cond)
+
+        print("====================loss==================")
+        print("model_output.size()", model_output.size())
 
         loss_dict = {}
         prefix = 'train' if self.training else 'val'
